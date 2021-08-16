46b54653fe524e3b0fd295f243dfc69f2d94fa30
K-C-Martin <57388565+K-C-Martin@users.noreply.github.com>
2021-08-16 15:02:25 +0200

    Add to libraries and improve documentation

--- 0 - libraries and functions.R
+++ 0 - libraries and functions.R
@@ -12,9 +12,16 @@
 
 # install if not already installed
 
-if (!require('data.table')) install.packages('data.table', dependencies = TRUE)  
-library('data.table')
+libs <- c('data.table',  # for efficiently handling large in-memory datasets
+          'tidyverse',   # for easy-to-read queries
+          'dtplyr')      # for translating tidyverse queries into stable, fast native data.table queries
 
+for (l in libs){
+  
+  if (!require(libs[l])) install.packages(libs[l], dependencies = TRUE)  
+  library(libs[l])
+  
+}
 
 
 
