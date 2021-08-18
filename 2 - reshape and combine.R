9add144f75809d64eddade65340d6afc8949ccea
K-C-Martin <57388565+K-C-Martin@users.noreply.github.com>
2021-08-19 00:30:40 +0200

    Attempt to calculate RMSE for each pset and variable

--- 2 - reshape and combine.R
+++ 2 - reshape and combine.R
@@ -68,7 +68,15 @@ num_psets <- dim(site_predict)[1] / num_pred_per_pset
 
 
 
+
+## LOOP THROUGH ALL SITE FILES
+
+#if (f != 1) fread
+
+
 ## CHECK ORDER OF VARIABLES MATCHES EXPECTATIONS, AND DISCARD 'VOLUME'
+
+# Check variable order
 
 var_order <- site_predict$variable[1:num_pred_per_pset]  # Expect var order pattern to repeat for each pset
 
@@ -78,14 +86,14 @@ msg <- paste0("Variable order in '", p_files[f], "' does not match expected orde
 if (!identical(var_order, expect_var_order)) stop(msg)
 
 
-# discard unneeded 'volume' variable
+# Discard unneeded 'volume' variable
 
 keep_var <- expect_var_order != 'volume'
 
 site_predict <- site_predict[rep(keep_var, times = num_psets), ]
 
 
-# update variable info
+# Update variable info
 
 expect_var <- expect_var[expect_var != 'volume']
 
@@ -95,8 +103,80 @@ num_pred_per_pset <- num_pred_per_var * num_var
 
 
 
+## CALCULATE SQUARED-ERROR FOR THIS SITE FOR ALL PSETS (COMBINE LATER TO CALCULATE RMSE FOR ALL PSETS)
 
-## CONVERT VALUES TO INTEGERS TO IMPROVE STORAGE EFFICIENCY 
+# Calculate squared error for all predictions
+
+site_predict$squared_error <- (site_predict$actual - site_predict$predicted)^2
+
+
+
+
+## CALCULATE RMSE FOR EACH PSET (ALL VARIABLES), AND FOR EACH VARIABLE IN EACH PSET
+
+# Calculate RMSE for all predictions per pset
+
+dt_conn <- lazy_dt(site_predict) # create a data.table connection to use dplyr
+
+pset_RMSE <- dt_conn %>%
+
+  select(-predicted, -actual) %>%
+
+  group_by(pset) %>%
+
+  summarise(n = n(), Sum_SE = sum(squared_error)) %>%
+
+  mutate(RMSE = sqrt(Sum_SE / n)) %>%
+
+  mutate(site = site_name) %>%
+
+  mutate(variable = 'all') %>% 
+  
+  select(pset, site, variable, RMSE) %>% 
+  
+  collect()
+
+
+# Calculate RMSE for all predictions per variable per pset
+
+dt_conn <- lazy_dt(site_predict) # create a data.table connection to use dplyr
+
+var_RMSE <- dt_conn %>% 
+  
+  select(-predicted, -actual) %>% 
+  
+  group_by(pset, variable) %>%
+  
+  summarise(n = n(), Sum_SE = sum(squared_error)) %>% 
+  
+  mutate(RMSE = sqrt(Sum_SE / n)) %>% 
+  
+  mutate(site = site_name) %>% 
+  
+  select(pset, site, variable, RMSE) %>% 
+  
+  collect()
+
+
+# Combine pset and variable-pset RMSE calculations and pivot wider
+
+pset_RMSE <- rbind(pset_RMSE, var_RMSE)
+
+pset_RMSE <- pset_RMSE %>% 
+  
+  pivot_wider(names_from = c(variable, site), values_from = RMSE)
+
+
+# Write RMSE values to file and remove objects
+
+fwrite()
+
+rm(var_RMSE)
+
+
+
+
+## CONVERT PREDICTED AND ACTUAL VALUES TO INTEGERS TO IMPROVE STORAGE EFFICIENCY 
 
 # Convert values to scaled integers (multiply by 1e5 to capture all decimal values)
 
@@ -105,25 +185,33 @@ site_predict$int_pred_e5 <- as.integer(site_predict$predicted * 1e5)
 site_predict$int_act_e5 <- as.integer(site_predict$actual * 1e5)
 
 
+# Remove numeric predicted and actual columns
+
+
+
+
+
 
 ## RESHAPE PREDICTIONS (PSETS COLUMN-WISE)
 
 # Reshape to one column for each pset
 
-dt_conn <- lazy_dt(site_predict) # create a data.table connection to use dplyr
-
-reshaped_pred <- dt_conn %>% 
-  
-  select(pset, site, date, variable, int_act_e5, int_pred_e5) %>% 
-  
-  pivot_wider(names_from = pset,
-              values_from = int_pred_e5) %>%  
-  
-  show_query()  # get native data.table query to run on data.table (see below)
+# dt_conn <- lazy_dt(site_predict) # create a data.table connection to use dplyr
+# 
+# reshaped_pred <- dt_conn %>% 
+#   
+#   select(pset, site, date, variable, int_act_e5, int_pred_e5) %>% 
+#   
+#   pivot_wider(names_from = pset,
+#               values_from = int_pred_e5) %>%  
+#   
+#   show_query()  # get native data.table query to run on data.table (see below)
 
 reshaped_pred <- dcast(site_predict[, .(pset, site, date, variable, int_act_e5, int_pred_e5)], 
                        formula = site + date + variable + int_act_e5 ~ pset, value.var = "int_pred_e5")
 
+# data.table with the following columns:
+# - 
 
 # NB: reshaped_predict rows are ordered by date, then variable name
 # NB: reshaped_predict pset columns are ordered alphabetically (pset1, pset10, pset100, ...)
@@ -158,13 +246,17 @@ msg <- "Pset row order in reshaped data.table does not match expectations."
 if (!identical(test$pset42, cf_pset$int_pred_e5)) stop(msg)
 
 
-# Remove original site predictions dt and connection object
-
-rm(site_predict)
-rm(dt_conn)
 
 
-#write.csv(reshaped_pred, 'output pset cols/test_output.csv', row.names = FALSE)
 
-#test_import <- fread('output pset cols/test_output.csv')
+
+# Remove original site predictions dt and connection object to free memory
+
+#rm(site_predict)
+#rm(dt_conn)
+
+
+# Save reshaped predictions for this site 
+
+#fwrite(reshaped_pred, paste0('output pset cols/', tstamp, '_', site_name, '_col_pset_pred.csv'))
 
