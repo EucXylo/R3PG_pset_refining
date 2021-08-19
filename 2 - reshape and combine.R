2ab4828f60c65709b378f1b23bed8e387f5390e1
K-C-Martin <57388565+K-C-Martin@users.noreply.github.com>
2021-08-19 02:47:17 +0200

    Source '2d - reshape psets to columns'

--- 2 - reshape and combine.R
+++ 2 - reshape and combine.R
@@ -93,7 +93,15 @@ for (f in seq_along(p_files[1])) {  # remove [1] to loop through multiple files!
   
   
   
+  # Convert values to scaled integers for efficiency (multiply by 1e5 to capture all decimal values)
+  
+  site_predict$int_pred_e5 <- as.integer(site_predict$predicted * 1e5)
+  
+  site_predict$int_act_e5 <- as.integer(site_predict$actual * 1e5)
 
+  
+  
+  # Reshape scaled integer predictions to column-wise (by pset)
 
   
   
@@ -105,74 +113,10 @@ for (f in seq_along(p_files[1])) {  # remove [1] to loop through multiple files!
 
 
 
-## CONVERT PREDICTED AND ACTUAL VALUES TO INTEGERS TO IMPROVE STORAGE EFFICIENCY 
-
-# Convert values to scaled integers (multiply by 1e5 to capture all decimal values)
-
-site_predict$int_pred_e5 <- as.integer(site_predict$predicted * 1e5)
-
-site_predict$int_act_e5 <- as.integer(site_predict$actual * 1e5)
-
-
-# Remove numeric predicted and actual columns
 
 
 
 
-
-
-## RESHAPE PREDICTIONS (PSETS COLUMN-WISE)
-
-# Reshape to one column for each pset
-
-# dt_conn <- lazy_dt(site_predict) # create a data.table connection to use dplyr
-# 
-# reshaped_pred <- dt_conn %>% 
-#   
-#   select(pset, site, date, variable, int_act_e5, int_pred_e5) %>% 
-#   
-#   pivot_wider(names_from = pset,
-#               values_from = int_pred_e5) %>%  
-#   
-#   show_query()  # get native data.table query to run on data.table (see below)
-
-reshaped_pred <- dcast(site_predict[, .(pset, site, date, variable, int_act_e5, int_pred_e5)], 
-                       formula = site + date + variable + int_act_e5 ~ pset, value.var = "int_pred_e5")
-
-# data.table with the following columns:
-# - 
-
-# NB: reshaped_predict rows are ordered by date, then variable name
-# NB: reshaped_predict pset columns are ordered alphabetically (pset1, pset10, pset100, ...)
-
-
-
-# Test psets columns are sorted alphabetically in reshaped dt
-
-pset_order  <- sort(paste0('pset', c(1:num_psets)))
-
-msg <- "Pset column order in reshaped data.table does not match expectations."
-if (!identical(colnames(reshaped_pred), c('site', 'date', 'variable', 'int_act_e5', pset_order))) stop(msg)
-
-
-
-# Test ordering by date and variable gives matching values for 'random' pset
-
-test <- reshaped_pred[, 'pset42']
-
-offset <- (42 - 1) * num_pred_per_pset
-
-cf_pset <- site_predict %>% 
-  
-  slice((1 + offset):(12 + offset)) %>% 
-  
-  arrange(date, variable) %>% 
-  
-  collect()
-
-
-msg <- "Pset row order in reshaped data.table does not match expectations."
-if (!identical(test$pset42, cf_pset$int_pred_e5)) stop(msg)
 
 
 
